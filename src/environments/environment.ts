6cce5c099fb6b18715ed0e2de5d2f82e27c1dd80
Soumaya.Mejri <Soumaya.Mejri@yesss.tn>
2020-05-03 21:46:31 +0100

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:
+  {
+    apiKey: "AIzaSyAUVT6aiRClrJenwIXEsni9RPdiShkg4x0",
+    authDomain: "ionic-4-firebase-login-3c79f.firebaseapp.com",
+    databaseURL: "https://ionic-4-firebase-login-3c79f.firebaseio.com",
+    projectId: "ionic-4-firebase-login-3c79f",
+    storageBucket: "ionic-4-firebase-login-3c79f.appspot.com",
+    messagingSenderId: "664132255547",
+    appId: "1:664132255547:web:35de93ab6f674aa940c11e"
+  }
 };
 
 /*
